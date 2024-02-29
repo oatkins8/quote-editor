2779724d063b77a5258fc6f7b63894228af5a6bb
Ollie Atkins <ollieatkins8@outlook.com>
2024-02-29 17:30:01 +0000

    Add seeds from fixtures
    Adds `rails db:fixtures:load` to seeds so that we can load
    fixtures from the command `rails db:seed`.
    
    Also moves seeds from spec to test as this is where it should've been
    to start!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/test/fixtures"
   config.global_fixtures = :all
 
   # prevent system tests opening a browser in chrome
